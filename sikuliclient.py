0a0657470a7135417d5ffb8172aebd82c78f233d
Alistair.broomhead <theheadofabroom@hotmail.com>
2012-11-27 12:36:28 +0000

    Written the basic functionality of sikuliclient.py

--- sikuliclient.py
+++ sikuliclient.py
@@ -2,6 +2,15 @@
 Client library for interacting with Siculi from Robotframework over XML-RPC
 """
 
+class RemoteLib(object):
+    def __init__(self, remote):self.__lib = remote
+    def __getattr__(self, name):
+            if name in self.__lib.get_keyword_names():
+                    def _(*args):return self.__lib.run_keyword(name, args)
+                    _.__name__ = name
+                    self.__dict__[name] = _
+                    return _
+            raise AttributeError()
 class SikuliClient(object):
     """
     Client for sikuliserver to expose a more limited set of keywords to rf
