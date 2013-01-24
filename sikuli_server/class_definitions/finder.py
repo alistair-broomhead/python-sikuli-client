04cdbc019019be98dcf032137f3a51f3905dc8e0
Al <alistair.broomhead@mindcandy.com>
2013-01-24 12:00:26 +0000

    Attempting to fit garbage collection

--- sikuli_server/class_definitions/finder.py
+++ sikuli_server/class_definitions/finder.py
@@ -3,9 +3,9 @@ Classes to fulfill the roles of those described at
     http://doc.sikuli.org/finder.html
 """
 __author__ = 'Alistair Broomhead'
-from .sikuli_class import UnimplementedSikuliClass
+from .sikuli_class import ServerSikuliClass
 
-class Finder(UnimplementedSikuliClass):
+class Finder(ServerSikuliClass):
     """ Manages interaction with Sikuli's Finder """
     #TODO: Finder
     # http://doc.sikuli.org/finder.html#Finder
--- sikuli_server/class_definitions/location.py
+++ sikuli_server/class_definitions/location.py
@@ -2,11 +2,11 @@
 Classes to fulfill the roles of those described at
     http://doc.sikuli.org/location.html
 """
-from .sikuli_class import UnimplementedSikuliClass
+from .sikuli_class import ServerSikuliClass
 
 __author__ = 'Alistair Broomhead'
 
-class Location(UnimplementedSikuliClass):
+class Location(ServerSikuliClass):
     """ Manages interaction with Sikuli's Location """
     #TODO: Location Objects
     # http://doc.sikuli.org/location.html#Location
