51ebb4b2d1f9a3404c7fb703d56221116a1e1fcf
Al <alistair.broomhead@mindcandy.com>
2013-01-25 12:12:35 +0000

    experimenting with auto-reflecting server classes where possible

--- sikuliserver.bat
+++ sikuliserver.bat
@@ -1,13 +1,14 @@
 SET OLDPATH=%PATH%
 ::
-:: These paths are the defaults for Sikuli X and Java 7 - if they're not
+:: These paths are the defaults for Sikuli X and Java 6 - if they're not
 :: correct, change them
 ::
 SET PATH=%OLDPATH%;C:\Program Files (x86)\Sikuli X\libs
-SET PATH=%OLDPATH%;C:\Program Files (x86)\Java\jre7\lib
+SET PATH=%OLDPATH%;C:\Program Files (x86)\Java\jre6\lib
+SET JAVA="C:\Program Files (x86)\Java\jre6\bin\java.exe"
 ::
 ::  This should already be set, but if not, uncomment it
 ::
 ::  SET SIKULI_HOME=C:\Program Files (x86)\Sikuli X\
-java -cp "%SIKULI_HOME%sikuli-script.jar" org.python.util.jython sikuliserver.py
+%JAVA% -cp "%SIKULI_HOME%sikuli-script.jar" org.python.util.jython sikuliserver.py
 SET PATH=%OLDPATH%
