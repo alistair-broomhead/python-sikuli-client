75d7eeaaf4355f162bd0da1d010ecafd88e44dcb
Al <alistair.broomhead@mindcandy.com>
2013-01-28 16:34:33 +0000

    docs should be sorted now

--- docs/build_help.sh
+++ docs/build_help.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-rm -rf _build
+make clean
 python regen.py
 make html
+if [ $? = 0 ]; then
+    # If the build is successful, add everything except for the source
+    # directory, which should be managed manually
+    set DOCSDIR=docs/html
+    git add $DOCSDIR/*
+    git add $DOCSDIR/_sources
+    git add $DOCSDIR/_static
+    git add $DOCSDIR/doctrees
+else
+    exit $?
+fi
